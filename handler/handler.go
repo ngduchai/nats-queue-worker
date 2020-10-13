38d6504ea97a175e8ff2fa358ec1e9d7fc032791
Hai Duc Nguyen <ndhai@river-fe3.cs.uchicago.edu>
2020-10-12 23:42:49 -0500

    update interface

--- handler/handler.go
+++ handler/handler.go
@@ -7,14 +7,17 @@ import (
 )
 
 // CreateNATSQueue ready for asynchronous processing
-func CreateNATSQueue(address string, port int, clusterName, channel string, clientConfig NATSConfig) (*NATSQueue, error) {
+//func CreateNATSQueue(address string, port int, clusterName, channel string, clientConfig NATSConfig) (*NATSQueue, error) {
+func CreateNATSQueue(address string, port int, clientConfig NATSConfig) (*NATSQueue, error) {
 	var err error
 	natsURL := fmt.Sprintf("nats://%s:%d", address, port)
 	log.Printf("Opening connection to %s\n", natsURL)
 
 	clientID := clientConfig.GetClientID()
+        clusterName := "faas-cluster";
 
 	// If 'channel' is empty, use the previous default.
+        channel := "";
 	if channel == "" {
 		channel = "faas-request"
 	}
